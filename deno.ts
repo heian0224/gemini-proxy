c2c7c19672f0f371ea19bf40eaf5ef8328167713
Cong <heian0224@gmail.com>
2024-01-17 10:34:36 +0800

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -2,13 +2,15 @@ import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 
 const DEEPAI_API_HOST = "generativelanguage.googleapis.com";
 
-serve(async (request) => {
+Deno.serve(async (request) => {
   const url = new URL(request.url);
-
-  if (url.pathname === "/") {
-    return fetch(new URL("./Readme.md", import.meta.url));
-  }
-
   url.host = DEEPAI_API_HOST;
-  return await fetch(url, request);
+
+  const newRequest = new Request(url.toString(), {
+    headers: request.headers,
+    method: request.method,
+    body: request.body,
+    redirect: "follow",
+  });
+  return await fetch(newRequest);
 });
